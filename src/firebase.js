e1f3d225b7df1a841e9f383098c69fbc14a6433a
AlexMasinde <56753745+AlexMasinde@users.noreply.github.com>
2021-07-22 11:53:40 +0300

    configured firebase

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const firestore = app.firestore();
+const auth = app.auth();
+const storage = app.storage();
+
+const database = {
+  users: firestore.collection("users"),
+  projects: firestore.collection("projects"),
+  courses: firestore.collection("courses"),
+  readings: firestore.collection("readings"),
+  projectTasks: firestore.collection("project-tasks"),
+  formatDocument: (doc) => {
+    return { id: doc.id, ...doc.data() };
+  },
+  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+};
+
+export { auth, storage, database };
