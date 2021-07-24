9a142cccc7f892c0246ac64aff70ac8e89c66f9f
AlexMasinde <56753745+AlexMasinde@users.noreply.github.com>
2021-07-24 07:32:30 +0300

    built pending tasks view component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import PendingTasks from "./components/PendingTasks/PendingTasks";
-
 function App() {
   return (
     <div className="App">
       <h1>Dev Scheduler</h1>
-      <PendingTasks />
     </div>
   );
 }
