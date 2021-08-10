6a683d3883397ed5b0b39be97d3324a9330c93b3
Jane Njeri <daktari2033@gmail.com>
2021-08-10 10:04:58 +0300

    commenced working on tests for activity modal

---
+++ src/components/AddActivityModal/__test__/AddActivityModal.test.js
@@ -0,0 +1,5 @@
+import { screen, render } from "@testing-library/react";
+
+import AddActivityModal from "../AddActivityModal";
+
+test("Test to ensure modal saves actvivity and closes when add button is closed", async () => {});
--- src/components/presentationcomponents/Input/Input.js
+++ src/components/presentationcomponents/Input/Input.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import InputStyles from "./Input.module.css";
 
-export default function Input({ placeholder, type, required, onChange }) {
+export default function Input({ placeholder, type, onChange }) {
   return (
     <input
       className={InputStyles.input}
       placeholder={placeholder}
-      required={required}
       type={type}
       onChange={onChange}
     />
