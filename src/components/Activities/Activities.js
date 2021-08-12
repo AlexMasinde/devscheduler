dbbad4852d7f9591b0458acd05417415be960a86
Jane Njeri <daktari2033@gmail.com>
2021-08-12 19:48:13 +0300

    commenced working on activities page

---
+++ src/components/Activities/Activities.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import ActivitiesStyles from './Activities.module.css'
+
+export default function Activities() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
