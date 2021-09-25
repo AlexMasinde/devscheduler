1a26c873b8f89784a9f0d326c640b405fada83d9
Jane Njeri <daktari2033@gmail.com>
2021-09-25 03:07:59 +0300

    reduced number of activities on display in homepage

--- src/components/ActivitiesList/ActivitiiesList.js
+++ src/components/ActivitiesList/ActivitiiesList.js
@@ -11,6 +11,8 @@ export default function ActivitiiesList() {
   const { activities, activeCategory } = useActivities();
   const title =
     activeCategory === "Home" ? "Latest Activities" : activeCategory;
+  const renderActivities =
+    activeCategory === "Home" ? activities.slice(0, 2) : activities;
 
   return (
     <div className={ActivitiiesListStyles.container}>
@@ -18,7 +20,7 @@ export default function ActivitiiesList() {
         <h1>{title}</h1>
       </div>
       <div>
-        {activities.map((activity) => {
+        {renderActivities.map((activity) => {
           return (
             <ActivityListItem activity={activity} key={shortid.generate()} />
           );
