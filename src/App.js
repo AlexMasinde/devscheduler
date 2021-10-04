0f9ee149511af8c9b195eb605109a74c9e63cc66
Jane Njeri <daktari2033@gmail.com>
2021-10-04 20:36:23 +0300

    routing;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Activities from "./pages/Activities/Activities";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch } from "react-router";
 
 import { ModalContextProvider } from "./contexts/modalContext";
 import { ActivitiesContextProvider } from "./contexts/activitiesContext";
 import { AddTaskModalContextProvider } from "./contexts/addtaskModalContext";
 import { DeleteModalContextProvider } from "./contexts/deleteModalContext";
-import UserForm from "./components/UserForm/UserForm";
 import { AuthContextProvider } from "./contexts/authContext";
+
+import Activities from "./pages/Activities/Activities";
+import UserForm from "./components/UserForm/UserForm";
 import Login from "./components/Login/Login";
 
 function App() {
